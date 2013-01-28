4ab8b81adf6368a9177e3401b2ff8e673d7a239b
Edward Kmett <ekmett@gmail.com>
2013-01-28 14:20:10 -0500

    Diagnostics, Meta, Syntax.

---
+++ src/Analytics/Diagnostic.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable #-}
+--------------------------------------------------------------------
+-- |
+-- Module    :  Analytics.Diagnostic
+-- Copyright :  (c) Edward Kmett and Dan Doel 2012-2013
+-- License   :  BSD3
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+-- This module re-exports 'Rendering' from @trifecta@ along with
+-- our in-house diagnostic type.
+--
+-- This allows the non-parsing modules to not have to concern
+-- themselves with how we choose to represent source locations.
+--------------------------------------------------------------------
+module Analytics.Diagnostic
+  (
+  -- * Rendering
+    Rendering
+  , HasRendering(..)
+  -- * Diagnostics
+  , Diagnostic(Diagnostic)
+  , AsDiagnostic(..)
+  , Err(Err)
+  , HasErr(..)
+  , die
+  ) where
+
+import Control.Exception
+import Control.Exception.Lens
+import Control.Lens
+import Data.Monoid
+import Data.Set
+import Data.Typeable
+import Text.PrettyPrint.ANSI.Leijen
+import Text.Trifecta.Rendering
+import Text.Trifecta.Result
+
+-- | Analytics diagnostic type
+data Diagnostic = Diagnostic !Rendering (Maybe Doc) [Doc] (Set String)
+  deriving Typeable
+
+-- | Construct a diagnostic
+die :: HasRendering r => r -> String -> Diagnostic
+die r s = Diagnostic (r^.rendering) (Just (pretty s)) [] mempty
+
+-- | This provides the '_Diagnostic' prism that can be used when matching against 'SomeException'
+-- or in a custom 'Error' type.
+class AsDiagnostic t where
+  _Diagnostic :: Prism' t Diagnostic
+
+instance AsDiagnostic Diagnostic where
+  _Diagnostic = id
+
+instance AsDiagnostic SomeException where
+  _Diagnostic = exception
+
+instance Exception Diagnostic
+
+instance Show Diagnostic where
+  show d = show $ plain $ explain (d^.rendering) (d^.err)
+
+instance HasErr Diagnostic where
+  err f (Diagnostic r p a x) = f (Err p a x) <&> \(Err q b y) -> Diagnostic r q b y
+
+instance HasRendering Diagnostic where
+  rendering f (Diagnostic r p a x) = f r <&> \r' -> Diagnostic r' p a x
---
+++ src/Analytics/Syntax.hs
@@ -0,0 +1,84 @@
+--------------------------------------------------------------------
+-- |
+-- Module    :  Analytics.Syntax
+-- Copyright :  (c) Edward Kmett 2012
+-- License   :  BSD3
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+--------------------------------------------------------------------
+module Analytics.Syntax
+  (
+  -- * Variables
+    Variable(..)
+  -- * Application
+  , App (..)
+  , apps
+  , (#)
+  -- * Fun
+  , Fun(..)
+  , (~>)
+  ) where
+
+import Control.Lens
+
+--------------------------------------------------------------------
+-- Variable
+--------------------------------------------------------------------
+
+-- | Discriminable 'pure'.
+class Variable t where
+  -- | If @t@ is also 'Applicative', then it should satisfy these laws:
+  --
+  -- @
+  -- 'pure' a '^?' 'var' ≡ 'Just' a
+  -- 'pure' ≡ 'review' 'var'
+  -- @
+  var :: Prism' (t a) a
+
+instance Variable Maybe where
+  var = _Just
+
+instance Variable [] where
+  var = prism return $ \xs -> case xs of
+    [x] -> Right x
+    _   -> Left xs
+
+instance Variable (Either b) where
+  var = _Right
+
+--------------------------------------------------------------------
+-- App
+--------------------------------------------------------------------
+
+-- | Discriminable 'App'
+--
+-- This prism provides ad hoc overloading of construction and pattern
+-- matching on 'App'.
+class App t where
+  app :: Prism' (t a) (t a, t a)
+
+infixl 9 ##
+
+-- | Convenient infix application operator.
+(##) :: App t => t a -> t a -> t a
+(##) = curry (review app)
+
+-- | Fold a series of applications.
+apps :: App t => t a -> [t a] -> t a
+apps = foldl (##)
+
+--------------------------------------------------------------------
+-- Fun
+--------------------------------------------------------------------
+
+-- | Discriminable syntactic arrows.
+class Fun t where
+  fun :: Prism' (t a) (t a,t a)
+
+infixr 0 ~>
+
+-- | Provide ad hoc overloading of function arrows.
+(~>) :: Fun t => t a -> t a -> t a
+(~>) = curry (review fun)
