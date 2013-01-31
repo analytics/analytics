d893439cc4f1e1cad5fa5539d79ebdf537eb2360
Edward Kmett <ekmett@gmail.com>
2013-01-31 04:29:37 -0500

    Top level docs for Data.Analytics.Prompt

--- src/Data/Analytics/Prompt.hs
+++ src/Data/Analytics/Prompt.hs
@@ -10,6 +10,13 @@
 -- Stability :  experimental
 -- Portability: non-portable
 --
+-- This module lets you step through a 'Datalog' program one 'Fact',
+-- rule or 'Query' at a time.
+--
+-- /Note:/ This module conflicts with @Data.Analytics.Datalog@ and
+-- @Data.Analytics.Internal.Datalog@ and the API is mostly of interest
+-- to query engine providers, so it is not re-exported through
+-- @Data.Analytics@.
 --------------------------------------------------------------------
 module Data.Analytics.Prompt
   ( Step(..)
@@ -24,15 +31,19 @@ import Data.Analytics.Match
 import Data.Analytics.Relation
 import Data.Typeable
 
+-- | A single 'Datalog' 'Fact', rule or 'Query'.
 data Step :: (* -> *) -> * -> * where
   Fact  :: (Typeable1 t, Match t) => (forall v. t v) -> Step m ()
   (:-)  :: Ord v => Relation v -> Query v t          -> Step m ()
   Query :: Ord v => Query v t                        -> Step m [t]
 
+-- | The result of prompting, after monadic effects.
 data Prompt :: (* -> *) -> * -> * where
   Done :: a -> Prompt m a
   (:>>=) :: Step m a -> (a -> Datalog m b) -> Prompt m b
 
+-- | Quotient out operational details of the Datalog program and get to the
+-- next 'Step'.
 prompt :: Monad m => Datalog m a -> m (Prompt m a)
 prompt (Datalog.Fact xs)          = return $ Fact xs :>>= return
 prompt (Datalog.Query q)          = return $ Query q :>>= return
