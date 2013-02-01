c733bdd8deb77212f5459644d8352e83b745dd64
Edward Kmett <ekmett@gmail.com>
2013-02-01 00:02:17 -0500

    Updated examples

--- examples/Closure.hs
+++ examples/Closure.hs
@@ -19,16 +19,18 @@ instance Variable Node where var = _Node
 instance Match Node where match = matchVar
 
 data Edge a = Edge (Node a) (Node a) deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable,Typeable,Generic)
-instance Match Edge where match f (Edge a b) (Edge c d) = Edge <$> match f a c <*> match f b d
+instance Match Edge where
+  match f (Edge a b) (Edge c d) = Edge <$> match f a c <*> match f b d
 edge :: Rel Edge a r => Node a -> Node a -> r
 edge x y = rel (Edge x y)
 
 data TC a = TC (Node a) (Node a) deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable,Typeable,Generic)
-instance Match TC where match f (TC a b) (TC c d) = TC <$> match f a c <*> match f b d
+instance Match TC where
+  match f (TC a b) (TC c d) = TC <$> match f a c <*> match f b d
 tc :: Rel TC a r => Node a -> Node a -> r
 tc x y = rel (TC x y)
 
-test :: Monad m => Datalog m [TC Void]
+test :: Monad m => Datalog m [TC a]
 test = do
   edge A B
   edge B C
