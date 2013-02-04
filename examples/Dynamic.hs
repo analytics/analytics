155f11012f2ff6a3c16d90f61acd4961ee857b61
Edward Kmett <ekmett@gmail.com>
2013-02-04 04:15:52 -0500

    MonadTable and Tabled lets us define dynamic tables as we run

---
+++ examples/Dynamic.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE TypeFamilies, TemplateHaskell, DeriveDataTypeable, FlexibleContexts, GADTs, Rank2Types #-}
+module Examples.Closure where
+
+import Control.Applicative
+import Control.Lens
+import Data.Analytics
+import Data.Foldable
+import Data.String
+import Data.Typeable
+import Data.Void
+import Generics.Deriving
+import Prelude.Extras
+
+data Node = A | B | C deriving (Eq,Ord,Show,Typeable)
+instance Term Node
+
+data NV = X | Y | Z deriving (Eq,Ord,Show,Typeable)
+instance Term NV where type Entity NV = Node; term = var
+
+data Edge = Edge Node Node deriving (Eq,Ord)
+
+test :: MonadTable t m => DatalogT t m [Edge]
+test = do
+  T2 edge <- table Edge
+  T2 tc <- table Edge
+  edge A B
+  edge B C
+  edge B A
+  tc X Y :- edge X Y
+  tc X Z :- tc X Y <* edge Y Z
+  query $ tc A X <* no (edge X C)
--- src/Data/Analytics/Table.hs
+++ src/Data/Analytics/Table.hs
@@ -1,15 +1,22 @@
-{-# LANGUAGE Rank2Types, MultiParamTypeClasses, TypeFamilies #-}
+{-# LANGUAGE Rank2Types, MultiParamTypeClasses, TypeFamilies, FunctionalDependencies, FlexibleInstances, UndecidableInstances #-}
 module Data.Analytics.Table
   ( T0, T1, T2, T3, T4
   , t0, t1, t2, t3, t4
   -- * Implementation Details
   , Atom
   , Atomic(..)
+  -- * Predicative Tables
+  , Table0(..), Table1(..), Table2(..), Table3(..), Table4(..)
+  , Tabled(..)
+  , MonadTable(..)
   ) where
 
 import Control.Applicative
+import Control.Monad.State.Lazy as Lazy
+import Control.Monad.State.Strict as Strict
 import Data.Analytics.Internal.Atom
 import Data.Analytics.Internal.Atomic
+import Data.Analytics.Internal.Datalog
 import Data.Analytics.Internal.Term
 
 type T0 t o         = forall r.          Atomic r t o
@@ -37,3 +44,54 @@ t3 t f a b c = atom t $ f <$> arg a <*> arg b <*> arg c
 
 t4 :: t -> (w -> x -> y -> z -> o) -> T4 t o w x y z
 t4 t f a b c d = atom t $ f <$> arg a <*> arg b <*> arg c <*> arg d
+
+------------------------------------------------------------------------------
+-- Useful for returning t0..t4 in a 'Monad' without @ImpredicativeTypes@.
+------------------------------------------------------------------------------
+
+data Table0 t o = T0 (T0 t o)
+data Table1 t o x = T1 (T1 t o x)
+data Table2 t o x y = T2 (T2 t o x y)
+data Table3 t o x y z = T3 (T3 t o x y z)
+data Table4 t o w x y z = T4 (T4 t o w x y z)
+
+------------------------------------------------------------------------------
+-- Tabled
+------------------------------------------------------------------------------
+
+class Tabled t k r | r -> t k where
+  table :: MonadTable t m => k -> m r
+
+instance Tabled t k (Table0 t k) where
+  table k = freshTable 0 >>= \t -> return $ T0 (t0 t k)
+
+instance Tabled t (x -> k) (Table1 t k x) where
+  table k = freshTable 1 >>= \t -> return $ T1 (t1 t k)
+
+instance Tabled t (x -> y -> k) (Table2 t k x y) where
+  table k = freshTable 2 >>= \t -> return $ T2 (t2 t k)
+
+instance Tabled t (x -> y -> z -> k) (Table3 t k x y z) where
+  table k = freshTable 3 >>= \t -> return $ T3 (t3 t k)
+
+instance Tabled t (w -> x -> y -> z -> k) (Table4 t k w x y z) where
+  table k = freshTable 4 >>= \t -> return $ T4 (t4 t k)
+
+------------------------------------------------------------------------------
+-- MonadTable
+------------------------------------------------------------------------------
+
+class Monad m => MonadTable t m | m -> t where
+  freshTable :: Int -> m t -- Build a table with a given arity
+
+instance MonadTable t m => MonadTable t (Lazy.StateT s m) where
+  freshTable = lift . freshTable
+  {-# INLINE freshTable #-}
+
+instance MonadTable t m => MonadTable t (Strict.StateT s m) where
+  freshTable = lift . freshTable
+  {-# INLINE freshTable #-}
+
+instance MonadTable t m => MonadTable t (DatalogT t m) where
+  freshTable = lift . freshTable
+  {-# INLINE freshTable #-}
