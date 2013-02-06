3ac92323fb6e849f44efb40644301b3ede077a23
Edward Kmett <ekmett@gmail.com>
2013-02-06 11:20:45 -0500

    inlining, ascii, iso8859_1

--- src/Data/Analytics/Storage/Schedule.hs
+++ src/Data/Analytics/Storage/Schedule.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE GADTs #-}
 module Data.Analytics.Storage.Schedule
   ( Schedule(..)
@@ -13,6 +14,9 @@ module Data.Analytics.Storage.Schedule
   , integral
   , bits
   , hashed
+  -- * Character schedules
+  , ascii
+  , iso8859_1
   ) where
 
 import Control.Lens
@@ -43,33 +47,87 @@ instance Contravariant Schedule where
 -- 256936680
 magic :: Int
 magic = 256936680
+{-# INLINE magic #-}
 
 integral :: forall a. (Storable a, Integral a) => Schedule a
 integral = contramap fromIntegral $ bits (sizeOf (undefined :: a) * 8)
+{-# INLINE integral #-}
 
 bits :: Int -> Schedule Int64
 bits n = Schedule 0 (if n <= 1 then 0 else div magic (n - 1)) n True False id
+{-# INLINE bits #-}
+
+-- We conservatively approximate characters above of this range by the upper bound to preserve ordering.
+ascii :: Schedule Char
+ascii = contramap (fromIntegral . clamp 127 . fromEnum) (bits 7)
+{-# INLINE ascii #-}
+
+clamp :: Int -> Int -> Int
+clamp n k
+  | k >= n = n
+  | otherwise = k
+{-# INLINE clamp #-}
+
+-- | We conservatively approximate characters above of this range by the upper bound to preserve ordering.
+iso8859_1 :: Schedule Char
+iso8859_1 = contramap (fromIntegral . clamp 255 . fromEnum) (bits 8)
+{-# INLINE iso8859_1 #-}
 
 hashed :: Hashable a => Schedule a
 hashed = contramap hash integral
   & scheduleOrdered .~ False
   & scheduleApproximate .~ True
+{-# INLINE hashed #-}
 
 -- | Retrieve the default key schedule for a type
 class Scheduled a where
   scheduled :: Schedule a
+  default scheduled :: (Storable a, Integral a) => Schedule a
+  scheduled = integral
 
-instance Scheduled Int where scheduled = integral
-instance Scheduled Int8 where scheduled = integral
-instance Scheduled Int16 where scheduled = integral
-instance Scheduled Int32 where scheduled = integral
-instance Scheduled Int64 where scheduled = bits 64
-instance Scheduled Word where scheduled = integral
-instance Scheduled Word8 where scheduled = integral
-instance Scheduled Word16 where scheduled = integral
-instance Scheduled Word32 where scheduled = integral
-instance Scheduled Word64 where scheduled = integral
+instance Scheduled Int where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Int8 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Int16 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Int32 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Int64 where
+  scheduled = bits 64
+  {-# INLINE scheduled #-}
+
+instance Scheduled Word where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Word8 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Word16 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Word32 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+instance Scheduled Word64 where
+  scheduled = integral
+  {-# INLINE scheduled #-}
+
+-- | You can also use 'ascii' and 'iso8859_1'
 instance Scheduled Char where
   scheduled = contramap (fromIntegral . fromEnum) (bits 21)
+  {-# INLINE scheduled #-}
 
 -- TODO: enumerated :: (Enum a, Bounded a) => Schedule a
