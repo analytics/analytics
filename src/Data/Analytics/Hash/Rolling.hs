fdc1d87827687e664fddf066a7308dcfecbd8137
Edward Kmett <ekmett@gmail.com>
2013-02-12 02:00:40 -0500

    Improved rolling hash. Removed MurmurHash

--- src/Data/Analytics/Hash/Rolling.hs
+++ src/Data/Analytics/Hash/Rolling.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Data.Analytics.Hash.Rolling
   ( rollingPrime
-  , rollingSum
-  , rollingSumXor
+  , rolling
   ) where
 
 import Data.Bits
@@ -11,6 +13,53 @@ import Data.ByteString.Lazy as L
 import Data.Monoid
 import Data.Int
 import Data.Word
+import Foreign.Storable
+import Foreign.Ptr
+import GHC.Base
+
+foreign import ccall "static &rolling_lut" rolling_lut :: Ptr Int32
+
+inlinePerformIO :: IO a -> a
+inlinePerformIO (IO m) = case m realWorld# of
+  (# _, r #) -> r
+{-# INLINE inlinePerformIO #-}
+
+lut :: Word8 -> Int32
+lut i = inlinePerformIO (peekElemOff rolling_lut (fromIntegral i))
+{-# INLINE lut #-}
+
+update :: Int32 -> Word8 -> Word8 -> Int32
+update hash x y = rotateR hash 1 `xor` lut x `xor` lut y
+{-# INLINE update #-}
+
+
+-- | Take a strict 'S.ByteString' and generate a new lazy 'L.ByteString' with chunks based on a rolling
+-- hash. This generates chunks with an expected size of 8k, where the sizes vary between 128 bytes and 64k each.
+-- and the breakpoints are based on moments where a rolling hash function applied to the last 128 bytes of the
+-- input matches a mask.
+--
+-- This can be used with various chunk hashing schemes to allow hashing that is fairly robust in the
+-- presence of inline insertions and deletions.
+--
+-- The rolling hash is based on the idea of buzhash, but since we have a known window size that is an integral
+-- multiple of the rotation window we save work.
+rolling :: L.ByteString -> L.ByteString
+rolling z = L.fromChunks $ go seed 0 z (L.unpack (L.replicate window 0 <> z)) (L.unpack z) where
+  go !h !c !bs (x:xs) (y:ys)
+    | ((h' .&. mask == mask) && c >= minSize) || c >= maxSize = case L.splitAt (c + 1) bs of
+       (l,r) -> B.concat (L.toChunks l) : go seed 0 r xs ys
+    | otherwise = go h' (c + 1) bs xs ys
+    where !h' = update h (if c < window then 0 else x) y
+  go _ _ bs _ _ = [B.concat $ L.toChunks bs]
+  mask     = 8191
+  minSize  = 128
+  maxSize  = 65536
+  window   = 128
+  seed     = 0 :: Int32
+{-# INLINE rolling #-}
+-- TODO: use bytestring internals to optimize the concatenations with their known length
+-- TODO: split out a separate init loop to run the minSize/window length to avoid the conditional.
+-- TODO: use bytestring internals to avoid constructing a bunch of intermediate lists.
 
 -- |
 -- Calculates @powqp m n p = 'rem' (m^n) p@ under the assumption that @m * p * p@ can fit in an @Int64@.
@@ -29,6 +78,8 @@ powqp m n p = case quotRem n 2 of
 --
 -- This can be used with various chunk hashing schemes to allow hashing that is fairly robust in the
 -- presence of inline insertions and deletions.
+--
+-- This scheme is based on the standard Rabin-Karp rolling checksum.
 rollingPrime :: L.ByteString -> L.ByteString
 rollingPrime z = L.fromChunks $ go seed 0 z (L.unpack (L.replicate window 0 <> z)) (L.unpack z) where
   go !h !c !bs (x:xs) (y:ys)
@@ -36,8 +87,8 @@ rollingPrime z = L.fromChunks $ go seed 0 z (L.unpack (L.replicate window 0 <> z
        (l,r) -> B.concat (L.toChunks l) : go seed 0 r xs ys
     | otherwise = go h' (c + 1) bs xs ys
     where
-      x' = if c < window then 0 else fromIntegral x
-      h' = rem (rem (h - magic_d * x') magic_q * magic_r + fromIntegral y) magic_q
+      !x' = if c < window then 0 else fromIntegral x
+      !h' = rem (rem (h - magic_d * x') magic_q * magic_r + fromIntegral y) magic_q
   go _ _ bs _ _ = [B.concat $ L.toChunks bs]
   magic_d = powqp magic_r (window-1) magic_q
   mask    = 8191
@@ -49,13 +100,14 @@ rollingPrime z = L.fromChunks $ go seed 0 z (L.unpack (L.replicate window 0 <> z
   seed = 0
 {-# INLINE rollingPrime #-}
 
+{-
 rollingSum :: L.ByteString -> L.ByteString
 rollingSum z = L.fromChunks $ go seed 0 z (L.unpack (L.replicate window 0 <> z)) (L.unpack z) where
   go !h !c !bs (x:xs) (y:ys)
     | ((h' .&. mask == mask) && c >= minSize) || c >= maxSize = case L.splitAt (c + 1) bs of
        (l,r) -> B.concat (L.toChunks l) : go seed 0 r xs ys
     | otherwise = go h' (c + 1) bs xs ys
-    where h' = h + fromIntegral y - if c < window then 0 else fromIntegral x
+    where !h' = h + fromIntegral y - if c < window then 0 else fromIntegral x
   go _ _ bs _ _ = [B.concat $ L.toChunks bs]
   mask     = 8191
   minSize  = 128
@@ -63,20 +115,38 @@ rollingSum z = L.fromChunks $ go seed 0 z (L.unpack (L.replicate window 0 <> z))
   window   = 128
   seed     = 0 :: Int
 {-# INLINE rollingSum #-}
+-}
 
-rollingSumXor :: L.ByteString -> L.ByteString
-rollingSumXor z = L.fromChunks $ go seed seed 0 z (L.unpack (L.replicate window 0 <> z)) (L.unpack z) where
+
+{-
+rolling :: L.ByteString -> L.ByteString
+rolling z = L.fromChunks $ go seed seed 0 z (L.unpack (L.replicate window 0 <> z)) (L.unpack z) where
   go !h !i !c !bs (x:xs) (y:ys)
     | ((h' .&. 127 == 127) && (i' .&. 63 == 63) && c >= minSize) || c >= maxSize = case L.splitAt (c + 1) bs of
        (l,r) -> B.concat (L.toChunks l) : go seed seed 0 r xs ys
     | otherwise = go h' i' (c + 1) bs xs ys
-    where 
-      x' = if c < window then 0 else x
-      h' = h + y - x'
-      i' = i `xor` y `xor` x'
+    where
+      !x' = if c < window then 0 else x
+      !h' = h + y - x'
+      !i' = i `xor` y `xor` x'
   go _ _ _ bs _ _ = [B.concat $ L.toChunks bs]
   minSize  = 128
   maxSize  = 65536
   window   = 128
   seed     = 0 :: Word8
 {-# INLINE rollingSumXor #-}
+-}
+
+{-
+unsafeCreate :: Int -> (Ptr Word8 -> IO ()) -> B.ByteString
+unsafeCreate l f = unsafeDupablePerformIO (create l f)
+{-# INLINE unsafeCreate #-}
+
+-- | Create ByteString of size @l@ and use action @f@ to fill it's contents.
+create :: Int -> (Ptr Word8 -> IO ()) -> IO B.ByteString
+create l f = do
+  fp <- mallocPlainForeignPtrBytes l
+  withForeignPtr fp $ \p -> f p
+  return $! BI.PS fp 0 l
+{-# INLINE create #-}
+-}
