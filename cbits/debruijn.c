9eefc1b7919390fbb8788c2f37a44e1fa6cde8ab
Edward Kmett <ekmett@gmail.com>
2013-02-19 04:12:23 -0500

    More debruijn multiplication tables

--- cbits/debruijn.c
+++ cbits/debruijn.c
@@ -1,6 +1,5 @@
 // A 64 bit deBruijn multiplication table
-
-unsigned char debruijn_lut[] = {
+unsigned char debruijn_lsb64[] = {
  63,  0, 58,  1, 59, 47, 53,  2,
  60, 39, 48, 27, 54, 33, 42,  3,
  61, 51, 37, 40, 49, 18, 28, 20,
@@ -9,4 +8,24 @@ unsigned char debruijn_lut[] = {
  50, 36, 17, 19, 29, 10, 13, 21,
  56, 45, 25, 31, 35, 16,  9, 12,
  44, 24, 15,  8, 23,  7,  6,  5
+};
+
+// A 32 bit deBruijn multiplication table for (n * 0x077CB531U) >> 27
+unsigned char debruijn_lsb32[] = {
+   0,  1, 28,  2, 29, 14, 24, 3,
+  30, 22, 20, 15, 25, 17,  4, 8,
+  31, 27, 13, 23, 21, 19, 16, 7,
+  26, 12, 18,  6, 11,  5, 10, 9
+};
+
+// Frigo's algorithm from http://stackoverflow.com/questions/7365562/de-bruijn-like-sequence-for-2n-1-how-is-it-constructed
+unsigned char debruijn_rank32[] = {
+  32, -1,  2, -1,  3, -1, -1, -1,
+  -1,  4, -1, 17, 13, -1, -1,  7,
+   0, -1, -1,  5, -1, -1, 27, 18,
+  29, 14, 24, -1, -1, 20,  8, -1,
+  31,  1, -1, -1, -1, 16, 12,  6,
+  -1, -1, -1, 26, 28, 23, 19, -1,
+  30, -1, 15, 11, -1, 25, 22, -1,
+  -1, 10, -1, 21,  9, -1, -1, -1
 };
--- src/Data/Analytics/Bits.hs
+++ src/Data/Analytics/Bits.hs
@@ -1,16 +1,97 @@
 {-# LANGUAGE CPP, ForeignFunctionInterface, MagicHash, UnboxedTuples #-}
 module Data.Analytics.Bits
-  ( lsb
-  , rank
+  ( Ranked(..)
+  , w32, w64
   ) where
 
 import Data.Bits
+import Data.Int
 import Data.Word
 import Foreign.Ptr
 import Foreign.Storable
 import GHC.Base
 
-foreign import ccall "static &debruijn_lut" debruijn_lut :: Ptr Word8
+class (Num t, Bits t) => Ranked t where
+  -- | Calculate the least significant set bit using a debruijn multiplication table.
+  -- /NB:/ The result of this function is undefined when given 0.
+  lsb :: t -> Int
+  lsb n = rank n - 1
+
+  -- | Calculate the number of trailing 0 bits.
+  rank :: t -> Int
+  rank 0 = 0
+  rank n = lsb n + 1
+
+instance Ranked Word64 where
+  lsb n = fromIntegral $ go (shiftR ((n .&. (-n)) * 0x07EDD5E59A4E28C2) 58) where
+    go :: Word64 -> Word8
+    go i = inlinePerformIO $ peekElemOff debruijn_lsb64 (fromIntegral i)
+  {-# INLINE lsb #-}
+
+instance Ranked Word32 where
+  lsb n = fromIntegral $ go (shiftR ((n .&. (-n)) * 0x077CB531) 27) where
+    go :: Word32 -> Word8
+    go i = inlinePerformIO $ peekElemOff debruijn_lsb32 (fromIntegral i)
+  rank n = fromIntegral $ go (shiftR ((n .&. (-n)) * 0x4279976B) 26) where
+    go :: Word32 -> Word8
+    go i = inlinePerformIO $ peekElemOff debruijn_rank32 (fromIntegral i)
+  {-# INLINE rank #-}
+
+instance Ranked Word16 where
+  lsb = lsb . w32
+  {-# INLINE lsb #-}
+  rank = lsb . w32
+  {-# INLINE rank #-}
+
+instance Ranked Word8 where
+  lsb = lsb . w32
+  {-# INLINE lsb #-}
+  rank = lsb . w32
+  {-# INLINE rank #-}
+
+instance Ranked Int64 where
+  lsb = lsb . w64
+  {-# INLINE lsb #-}
+  rank = lsb . w64
+  {-# INLINE rank #-}
+
+instance Ranked Int32 where
+  lsb = lsb . w32
+  {-# INLINE lsb #-}
+  rank = lsb . w32
+  {-# INLINE rank #-}
+
+instance Ranked Int16 where
+  lsb = lsb . w32
+  {-# INLINE lsb #-}
+  rank = lsb . w32
+  {-# INLINE rank #-}
+
+instance Ranked Int8 where
+  lsb = lsb . w32
+  {-# INLINE lsb #-}
+  rank = lsb . w32
+  {-# INLINE rank #-}
+
+------------------------------------------------------------------------------
+-- Util
+------------------------------------------------------------------------------
+
+w32 :: Integral a => a -> Word32
+w32 = fromIntegral
+{-# INLINE w32 #-}
+
+w64 :: Integral a => a -> Word64
+w64 = fromIntegral
+{-# INLINE w64 #-}
+
+------------------------------------------------------------------------------
+-- de Bruijn Multiplication Tables
+------------------------------------------------------------------------------
+
+foreign import ccall "static &debruijn_lut" debruijn_lsb64  :: Ptr Word8
+foreign import ccall "static &debruijn_lut" debruijn_lsb32  :: Ptr Word8
+foreign import ccall "static &debruijn_lut" debruijn_rank32 :: Ptr Word8
 
 #ifndef HLINT
 inlinePerformIO :: IO a -> a
@@ -18,18 +99,3 @@ inlinePerformIO (IO m) = case m realWorld# of
   (# _, r #) -> r
 {-# INLINE inlinePerformIO #-}
 #endif HLINT
-
-debruijn :: Word64 -> Word8
-debruijn i = inlinePerformIO (peekElemOff debruijn_lut (fromIntegral i))
-{-# INLINE debruijn #-}
-
--- | Calculate the least significant set bit using a debruijn multiplication table
-lsb :: Word64 -> Int
-lsb n = fromIntegral $ debruijn (shiftR ((n .&. (-n)) * 0x07EDD5E59A4E28C2) 58)
-{-# INLINE lsb #-}
-
--- | Calculate the 1 + the index of the least significant bit, returning 0 if no bit is set
-rank :: Word64 -> Int
-rank 0 = 0
-rank n = lsb n + 1
-{-# INLINE rank #-}
