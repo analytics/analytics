6ae4a71d1814412f793a8bd7116ad90ad5b246ce
Edward Kmett <ekmett@gmail.com>
2013-02-21 02:57:32 -0500

    Updated Relations to use a Map

--- src/Data/Analytics/Datalog/Evaluation/Naive.hs
+++ src/Data/Analytics/Datalog/Evaluation/Naive.hs
@@ -5,31 +5,31 @@ module Data.Analytics.Datalog.Evaluation.Naive
   , insert
   ) where
 
+import Control.Applicative
 import Control.Lens
+import Control.Monad
 import Data.Analytics.Datalog.Atom
 import Data.Analytics.Datalog.Row
 import Data.Maybe
-import Data.IntMap
+import Data.IntMap as IntMap hiding (insert)
+import Data.Map as Map hiding (insert)
 import Data.Semigroup
 import Data.Typeable
 
 data Relation where
-  Relation :: (Typeable a, Typeable b) => [(Row (a -> b), a -> b, a)] -> Relation
+  Relation :: (Typeable a, Typeable b) => Map (Row (a -> b)) a -> Relation
   deriving Typeable
 
 rows :: (Typeable a, Typeable b) => Atom Int a b -> IntMap Relation -> [b]
 rows (Atom t r) m = case m^.at t of
   Nothing -> []
-  Just (Relation rl) -> fromMaybe (error "wibble") $ cast [ f a | (r',f,a) <- rl, matches r r' ]
+  Just (Relation rl) -> fromMaybe (error "wibble") $ cast $ do
+     (r', a) <- Map.toList rl
+     guard $ matches r r'
+     f <- maybeToList (runRow r')
+     return (f a)
 
 insert :: (Typeable a, Typeable b, Semigroup a) => Atom Int a b -> a -> IntMap Relation -> IntMap Relation
-insert (Atom t r) a m = case runRow r of
-  Nothing -> error "wobble"
-  Just f -> m & at t %~ relate f
- where
-    relate f Nothing   = Just $ Relation [(r,f,a)]
-    relate f (Just (Relation rl)) = Just $ Relation $ go f $ fromMaybe (error "twubble") $ cast rl
-    go f [] = [(r,f,a)]
-    go f (rgb@(r',g,b):xs)
-      | matches r r' = (r',g,b <> a):xs
-      | otherwise    = rgb:go f xs
+insert (Atom t r) a m = m & at t %~ \ys -> case ys of
+  Nothing            -> Just $! Relation (Map.singleton r a)
+  Just (Relation rm) -> cast rm <&> \rn -> Relation $ rn & at r %~ \xs -> fmap (<> a) xs <|> Just a
