e7dc218863e1fa88fb8ed041f683ea53eb2890d0
Edward Kmett <ekmett@gmail.com>
2013-02-21 03:01:44 -0500

    Generalize over the Relation index

--- src/Data/Analytics/Datalog/Evaluation/Naive.hs
+++ src/Data/Analytics/Datalog/Evaluation/Naive.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GADTs, DeriveDataTypeable #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module Data.Analytics.Datalog.Evaluation.Naive
   ( Relation(..)
   , rows
@@ -11,7 +12,6 @@ import Control.Monad
 import Data.Analytics.Datalog.Atom
 import Data.Analytics.Datalog.Row
 import Data.Maybe
-import Data.IntMap as IntMap hiding (insert)
 import Data.Map as Map hiding (insert)
 import Data.Semigroup
 import Data.Typeable
@@ -20,7 +20,7 @@ data Relation where
   Relation :: (Typeable a, Typeable b) => Map (Row (a -> b)) a -> Relation
   deriving Typeable
 
-rows :: (Typeable a, Typeable b) => Atom Int a b -> IntMap Relation -> [b]
+rows :: (At r, Typeable a, Typeable b, IxValue r ~ Relation) => Atom (Index r) a b -> r -> [b]
 rows (Atom t r) m = case m^.at t of
   Nothing -> []
   Just (Relation rl) -> fromMaybe (error "wibble") $ cast $ do
@@ -29,7 +29,7 @@ rows (Atom t r) m = case m^.at t of
      f <- maybeToList (runRow r')
      return (f a)
 
-insert :: (Typeable a, Typeable b, Semigroup a) => Atom Int a b -> a -> IntMap Relation -> IntMap Relation
+insert :: (At r, Typeable a, Typeable b, Semigroup a, IxValue r ~ Relation) => Atom (Index r) a b -> a -> r -> r
 insert (Atom t r) a m = m & at t %~ \ys -> case ys of
   Nothing            -> Just $! Relation (Map.singleton r a)
   Just (Relation rm) -> cast rm <&> \rn -> Relation $ rn & at r %~ \xs -> fmap (<> a) xs <|> Just a
