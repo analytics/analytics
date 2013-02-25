fb576173fd57e3c1d1212e337f20867bdf1d554b
Edward Kmett <ekmett@gmail.com>
2013-02-25 07:30:56 -0500

    Exposed Observable via Data.Analytics.Active

--- src/Data/Analytics/Active.hs
+++ src/Data/Analytics/Active.hs
@@ -7,10 +7,14 @@ module Data.Analytics.Active
   , (|>>)
   -- * Observer
   , Observer(..)
+  -- * Observable
+  , Observable(..)
+  , safe, fby, never, observe
   -- * Subscription
   , Subscription(..)
   ) where
 
 import Data.Analytics.Active.Observer
+import Data.Analytics.Active.Observable
 import Data.Analytics.Active.Task
 import Data.Analytics.Active.Subscription
