216c30d68e73d2daeda283a581fef46bfe1eaac4
Edward Kmett <ekmett@gmail.com>
2013-02-28 17:30:59 -0500

    Use the continuous variation on Schraudolph's better_exp recommendation to improve the accuracy of `fexp` and `fpow` dramatically.

--- src/Data/Analytics/Numeric/Fast.hs
+++ src/Data/Analytics/Numeric/Fast.hs
@@ -17,6 +17,8 @@
 module Data.Analytics.Numeric.Fast
   ( Fast(..)
   , blog
+  , expf_fast
+  , powf_fast_precise
   ) where
 
 class Floating a => Fast a where
@@ -37,10 +39,10 @@ instance Fast Float where
   flog     = logf_fast
   flog_lb  = logf_fast_lb
   flog_ub  = logf_fast_ub
-  fexp     = expf_fast
+  fexp     = better_expf_fast
   fexp_lb  = expf_fast_lb
   fexp_ub  = expf_fast_ub
-  fpow     = powf_fast_precise
+  fpow     = better_powf_fast_precise
   fpow_lb  = powf_fast_lb
   fpow_ub  = powf_fast_ub
 
@@ -48,10 +50,10 @@ instance Fast Double where
   flog     = log_fast
   flog_lb  = log_fast_lb
   flog_ub  = log_fast_ub
-  fexp     = exp_fast
+  fexp     = better_exp_fast
   fexp_lb  = exp_fast_lb
   fexp_ub  = exp_fast_ub
-  fpow     = pow_fast_precise
+  fpow     = better_pow_fast_precise
   fpow_lb  = pow_fast_lb
   fpow_ub  = pow_fast_ub
 
@@ -59,20 +61,24 @@ instance Fast Double where
 --
 -- This is a remarkably bad approximate logarithm.
 --
--- 'flog' should outperform it! It is provided merely for comparison.
+-- 'flog' had better outperform it! It is provided merely for comparison.
 blog :: Floating a => a -> a
-blog x = 6 * (x - 1) / (x + 1 + 4 * sqrt(x));
+blog x = 6 * (x - 1) / (x + 1 + 4 * sqrt x);
 
 foreign import ccall unsafe pow_fast_lb       :: Double -> Double -> Double
 foreign import ccall unsafe pow_fast_ub       :: Double -> Double -> Double
 foreign import ccall unsafe pow_fast_precise  :: Double -> Double -> Double
+foreign import ccall unsafe better_pow_fast_precise  :: Double -> Double -> Double
 foreign import ccall unsafe powf_fast_lb      :: Float -> Float -> Float
 foreign import ccall unsafe powf_fast_ub      :: Float -> Float -> Float
 foreign import ccall unsafe powf_fast_precise :: Float -> Float -> Float
+foreign import ccall unsafe better_powf_fast_precise :: Float -> Float -> Float
 
+foreign import ccall unsafe better_exp_fast   :: Double -> Double
 foreign import ccall unsafe exp_fast          :: Double -> Double
 foreign import ccall unsafe exp_fast_lb       :: Double -> Double
 foreign import ccall unsafe exp_fast_ub       :: Double -> Double
+foreign import ccall unsafe better_expf_fast  :: Float -> Float
 foreign import ccall unsafe expf_fast         :: Float -> Float
 foreign import ccall unsafe expf_fast_lb      :: Float -> Float
 foreign import ccall unsafe expf_fast_ub      :: Float -> Float
