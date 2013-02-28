99d9e0f51938a956435ad63257407ed996d1c4f3
Edward Kmett <ekmett@gmail.com>
2013-02-28 02:06:09 -0500

    Computed upper and lower bounds on all Double-based approximate algorithms.

--- cbits/fast.c
+++ cbits/fast.c
@@ -1,28 +1,45 @@
 /*
- See http://martin.ankerl.com/2007/10/04/optimized-pow-approximation-for-java-and-c-c/
-
- All of these rely on being on a little endian machine, such as an Intel box.
-
- These can be _quite_ inaccurate. ~20% in many cases, but being much faster (~7x) may
- permit more loop iterations of tuning algorithms that only need approximate powers.
-
- --Edward Kmett
-
- TODO: Incorporate the changes suggested by Nic Schraudolph to Ankerl's inversion:
-
- "You can get a much better approximation (piecewise rational instead of linear) at the cost of a single floating-point division by using better_exp(x) = exp(x/2)/exp(-x/2), where exp() is my published approximation but you don't need the additive constant anymore, you can use c=0. On machines with hardware division this is very attractive." -- Nic Schraudolph
-
- TODO: Incorporate the techniques from https://code.google.com/p/fastapprox/ to enable us
-to calculate more interesting approximate functions. They might need to be generalized to work on
-Double values where appropriate I suppose.
-*/
+ * See http://martin.ankerl.com/2007/10/04/optimized-pow-approximation-for-java-and-c-c/
+ *
+ * All of these rely on being on a little endian machine, such as an Intel box.
+ *
+ * These can be _quite_ inaccurate. ~20% in many cases, but being much faster (~7x) may
+ * permit more loop iterations of tuning algorithms that only need approximate powers.
+ *
+ * This version of Ankerl's algorithm has been extended to provide optionally conservative (lower) bounds
+ * and also to generate a full linear interpolation across the entire significand rather than 'stair-step'
+ * at the expense of performing a 64 bit operation rather than a 32 bit one. This is cheap these days.
+ *
+ * --Edward Kmett
+ *
+ * TODO: Incorporate the changes suggested by Nic Schraudolph to Ankerl's inversion:
+ *
+ * "You can get a much better approximation (piecewise rational instead of linear) at
+ * the cost of a single floating-point division by using better_exp(x) = exp(x/2)/exp(-x/2),
+ * where exp() is my published approximation but you don't need the additive constant anymore,
+ * you can use c=0. On machines with hardware division this is very attractive." -- Nic Schraudolph
+ *
+ * TODO: Incorporate the techniques from https://code.google.com/p/fastapprox/ to enable us
+ * to calculate more interesting approximate functions. They might need to be generalized to work on
+ * Double values where appropriate I suppose.
+ *
+ * Magic numbers:
+ * float /int      : round(1<<23/log(2)) = 12102203,          127<<23 = 1065353216
+ * double/int      : round(1<<20/log(2)) = 1512775,          1023<<20 = 1072693248
+ * double/long long: round(1<<52/log(2)) = 6497320848556798, 1023<<52 = 4607182418800017408
+ *
+ * Fudge factors such that exp y <= exp_fast y
+ * >>> ceiling (2^23 * (1 - (log (log 2) + 1)/log 2))
+ * 722019
+ * >>> ceiling (2^20 * (1 - (log (log 2) + 1)/log 2))
+ * 90253
+ * >>> ceiling (2^52 * (1 - (log (log 2) + 1)/log 2))
+ * 387630818974388
+ */
 
 // Ankerl's version of Schraudolph's approximation.
 double pow_fast(double a, double b) {
-  union {
-    double d;
-    int x[2];
-  } u = { a };
+  union { double d; int x[2]; } u = { a };
   u.x[1] = (int)(b * (u.x[1] - 1072632447) + 1072632447);
   u.x[0] = 0;
   return u.d;
@@ -31,16 +48,10 @@ double pow_fast(double a, double b) {
 /*
   "I have used the same trick for float, not double, with some slight modification to the constants to suite IEEE754 float format. The first constant for float is 1<<23/log(2) and the second is 127<<23 (for double they are 1<<20/log(2) and 1023<<20)." -- John
 
-
- double approximation: round(1<<20/log(2)) = 1512775, 1023<<20 = 1072693248
- float approximation: round(1<<23/log(2)) = 12102203, 127<<23 = 1065353216
 */
 
 float powf_fast(float a, float b) {
-  union {
-    float d;
-    int x;
-  } u = { a };
+  union { float d; int x; } u = { a };
   u.x = (int)(b * (u.x - 1065353216) + 1065353216);
   return u.d;
 }
@@ -51,19 +62,28 @@ float powf_fast(float a, float b) {
  double long long approximation: round 1<<52/log(2) 6497320848556798,
   mask = 0x3ff0000000000000LL = 4607182418800017408LL
 
-
 >>> round (2**52 * log (3 / (8 * log 2) + 1/2) / log 2 - 1/2)
 261140389990638
 >>> 0x3ff0000000000000 - round (2**52 * log (3 / (8 * log 2) + 1/2) / log 2 - 1/2)
 4606921278410026770
 
 */
+
+double pow_fast_smooth_upper_bound(double a, double b) {
+  union { double d; long long x; } u = { a };
+  u.x = (long long)(b * (u.x - 4606794787981043020LL) + 4607182418800017409LL);
+  return u.d;
+}
+
 double pow_fast_smooth(double a, double b) {
-  union {
-    double d;
-    long long x;
-  } u = { a };
-  u.x = (long long)(b * (u.x - 0x3fef127e83d16f12LL) + 0x3fef127e83d16f12LL);
+  union { double d; long long x; } u = { a };
+  u.x = (long long)(b * (u.x - 4606921278410026770LL) + 4606921278410026770LL);
+  return u.d;
+}
+
+double pow_fast_smooth_lower_bound(double a, double b) {
+  union { double d; long long x; } u = { a };
+  u.x = (long long)(b * (u.x - 4607182418800017409LL) + 4606794787981043020LL);
   return u.d;
 }
 
@@ -77,10 +97,7 @@ double pow_fast_precise(double a, double b) {
 
   /* calculate approximation with fraction of the exponent */
   int e = (int) b;
-  union {
-    double d;
-    int x[2];
-  } u = { a };
+  union { double d; int x[2]; } u = { a };
   u.x[1] = (int)((b - e) * (u.x[1] - 1072632447) + 1072632447);
   u.x[0] = 0;
 
@@ -107,10 +124,7 @@ double pow_fast_precise_smooth(double a, double b) {
 
   /* calculate approximation with fraction of the exponent */
   int e = (int) b;
-  union {
-    double d;
-    long long x;
-  } u = { a };
+  union { double d; long long x; } u = { a };
   u.x = (long long)((b - e) * (u.x - 0x3fef127e83d16f12LL) + 0x3fef127e83d16f12LL);
 
   double r = 1.0;
@@ -137,10 +151,7 @@ float powf_fast_precise(float a, float b) {
 
   /* calculate approximation with fraction of the exponent */
   int e = (int) b;
-  union {
-    float f;
-    int x;
-  } u = { a };
+  union { float f; int x; } u = { a };
   u.x = (int)((b - e) * (u.x - 1065353216) + 1065353216);
 
   float r = 1.0f;
@@ -161,21 +172,41 @@ float powf_fast_precise(float a, float b) {
   mask = 0x3ff0000000000000LL = 4607182418800017408LL
  double approximation: round(1<<20/log(2)) = 1512775, 1023<<20 = 1072693248
 */
+
+/* 4607182418800017408 - 387630818974388 = 4606794787981043020
+
+Exponent mask adapted to full 64 bit precision:
+>>> 1023 * 2^52
+4607182418800017408
+
+The fudge factor for conservative lower bound adapted to full 64 bit precision:
+>>> round (2^52 * (1 - (log (log 2) + 1)/log 2))
+387630818974388
+
+As a lower bound this is suitable for use when generating Mass and Precision estimates.
+*/
+double exp_fast_smooth_lower_bound(double a) {
+  union { double d; long long x; } u;
+  u.x = (long long)(6497320848556798LL * a + 4606794787981043020);
+  return u.d;
+}
+
+/* 4607182418800017408 + 1 */
+double exp_fast_smooth_upper_bound(double a) {
+  union { double d; long long x; } u;
+  u.x = (long long)(6497320848556798LL * a + 4607182418800017409);
+  return u.d;
+}
+
 double exp_fast_smooth(double a) {
-  union {
-    double d;
-    long long x;
-  } u;
+  union { double d; long long x; } u;
   u.x = (long long)(6497320848556798LL * a + 0x3fef127e83d16f12LL);
   return u.d;
 }
 
 /* Schraudolph's published algorithm */
 double exp_fast(double a) {
-  union {
-    double d;
-    int x[2];
-  } u;
+  union { double d; int x[2]; } u;
   u.x[1] = (int) (1512775 * a + 1072632447);
   u.x[0] = 0;
   return u.d;
@@ -184,30 +215,31 @@ double exp_fast(double a) {
 
 /* Schraudolph's published algorithm with John's constants */
 float expf_fast(float a) {
-  union {
-    float f;
-    int x;
-  } u;
+  union { float f; int x; } u;
   u.x = (int) (12102203 * a + 1065353216);
   return u.f;
 }
 
-/* Ankerl's inversion of Schraudolph's published algorithm */
+
+/* Ankerl's inversion of Schraudolph's published algorithm, converted to explicit multiplication */
 double log_fast(double a) {
-  union {
-    double d;
-    int x[2];
-  } u = { a };
-  return (u.x[1] - 1072632447) / 1512775.0;
+  union { double d; int x[2]; } u = { a };
+  return (u.x[1] - 1072632447) * 6.610368362777016e-7; // 1 / 1512775.0;
 }
 
+
+double log_fast_smooth_upper_bound(double a) {
+  union { double d; long long x; } u = { a };
+  return (u.x - 4606794787981043020) * 1.539095918623324e-16; // 1 / 6497320848556798.0;
+}
 /* Ankerl's inversion of Schraudolph's published algorithm with my constants */
 double log_fast_smooth(double a) {
-  union {
-    double d;
-    long long x;
-  } u = { a };
-  return (u.x - 0x3fef127e83d16f12LL) / 6497320848556798.0;
+  union { double d; long long x; } u = { a };
+  return (u.x - 4606921278410026770) * 1.539095918623324e-16; // 1 / 6497320848556798.0;
+}
+double log_fast_smooth_lower_bound(double a) {
+  union { double d; long long x; } u = { a };
+  return (u.x - 4607182418800017409) * 1.539095918623324e-16; // 1 / 6497320848556798.0;
 }
 
 /* Ankerl's adaptation of Schraudolph's published algorithm with John's constants */
@@ -216,5 +248,7 @@ float logf_fast(float a) {
     float f;
     int x;
   } u = { a };
-  return (u.x - 1065353216) / 12102203.0;
+  return (u.x - 1065353216) * 8.262958405176314e-8f; // / 12102203.0;
 }
+
+/*  722019 */
--- src/Data/Analytics/Numeric/Fast.hs
+++ src/Data/Analytics/Numeric/Fast.hs
@@ -26,39 +26,57 @@ module Data.Analytics.Numeric.Fast
 
 class Floating a => Fast a where
   -- | Calculate an approximate log.
-  flog  :: a -> a
+  flog    :: a -> a
+  flog_lb :: a -> a
+  flog_ub :: a -> a
   -- | Calculate an approximate exp.
-  fexp  :: a -> a
+  fexp    :: a -> a
+  fexp_lb :: a -> a
+  fexp_ub :: a -> a
   -- | Calculate an approximate pow.
-  fpow :: a -> a -> a
+  fpow    :: a -> a -> a
+  fpow_lb :: a -> a -> a
+  fpow_ub :: a -> a -> a
 
 instance Fast Float where
-  flog  = logf_fast
-  fexp  = expf_fast
+  flog = logf_fast
+  fexp = expf_fast
   fpow = powf_fast_precise
 
 instance Fast Double where
-  flog  = log_fast_smooth
-  fexp  = exp_fast_smooth
-  fpow = pow_fast_precise_smooth
+  flog     = log_fast_smooth
+  flog_lb  = log_fast_smooth_lower_bound
+  flog_ub  = log_fast_smooth_upper_bound
+  fexp     = exp_fast_smooth
+  fexp_lb  = exp_fast_smooth_lower_bound
+  fexp_ub  = exp_fast_smooth_upper_bound
+  fpow     = pow_fast_precise_smooth
+  fpow_lb  = pow_fast_smooth_lower_bound
+  fpow_ub  = pow_fast_smooth_upper_bound
 
 -- | Borchardt’s Algorithm from “Dead Reckoning: Calculating without instruments”.
 --
 -- This is a remarkably bad approximate logarithm.
 --
--- 'flog' should outperform it. It is provided merely for comparison.
+-- 'flog' should outperform it! It is provided merely for comparison.
 blog :: Floating a => a -> a
 blog x = 6 * (x - 1) / (x + 1 + 4 * sqrt(x));
 
 foreign import ccall unsafe pow_fast  :: Double -> Double -> Double
 foreign import ccall unsafe pow_fast_smooth  :: Double -> Double -> Double
+foreign import ccall unsafe pow_fast_smooth_lower_bound  :: Double -> Double -> Double
+foreign import ccall unsafe pow_fast_smooth_upper_bound  :: Double -> Double -> Double
 foreign import ccall unsafe powf_fast :: Float -> Float -> Float
 foreign import ccall unsafe pow_fast_precise  :: Double -> Double -> Double
 foreign import ccall unsafe pow_fast_precise_smooth  :: Double -> Double -> Double
 foreign import ccall unsafe powf_fast_precise :: Float -> Float -> Float
 foreign import ccall unsafe exp_fast  :: Double -> Double
 foreign import ccall unsafe exp_fast_smooth :: Double -> Double
+foreign import ccall unsafe exp_fast_smooth_lower_bound :: Double -> Double
+foreign import ccall unsafe exp_fast_smooth_upper_bound :: Double -> Double
 foreign import ccall unsafe expf_fast :: Float -> Float
 foreign import ccall unsafe log_fast  :: Double -> Double
 foreign import ccall unsafe log_fast_smooth :: Double -> Double
+foreign import ccall unsafe log_fast_smooth_lower_bound :: Double -> Double
+foreign import ccall unsafe log_fast_smooth_upper_bound :: Double -> Double
 foreign import ccall unsafe logf_fast :: Float -> Float
