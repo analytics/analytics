4debc6f08e1570e233998109c5ef3e1c7241c1a2
Edward Kmett <ekmett@gmail.com>
2013-03-07 05:33:53 -0500

    Added CRC32

---
+++ src/Data/Analytics/Hash/CRC32.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+module Data.Analytics.Hash.CRC32
+  ( CRC32
+  , initial
+  , update, updated
+  , final
+  ) where
+
+
+import Control.Lens
+import Data.Bits
+import Data.Monoid
+import Data.Word
+import Foreign.Storable
+import Foreign.Ptr
+import GHC.Base
+
+------------------------------------------------------------------------------
+-- CRC32
+------------------------------------------------------------------------------
+
+newtype CRC32 = CRC32 { getCRC32 :: Word32 }
+
+initial :: CRC32
+initial = CRC32 0xffffffff;
+{-# INLINE initial #-}
+
+update :: Word8 -> CRC32 -> CRC32
+update w (CRC32 h) = CRC32 (shiftL h 8 `xor` lut w)
+{-# INLINE update #-}
+
+updated :: Getting (Endo CRC32) t Word8 -> t -> CRC32 -> CRC32
+updated l t z = foldrOf l update z t
+{-# INLINE updated #-}
+
+final :: CRC32 -> Word32
+final = complement . getCRC32
+{-# INLINE final #-}
+
+------------------------------------------------------------------------------
+-- Utilities
+------------------------------------------------------------------------------
+
+foreign import ccall "static &crc32_lut" crc32_lut :: Ptr Word32
+
+#ifndef HLINT
+inlinePerformIO :: IO a -> a
+inlinePerformIO (IO m) = case m realWorld# of
+  (# _, r #) -> r
+{-# INLINE inlinePerformIO #-}
+#endif
+
+lut :: Word8 -> Word32
+lut i = inlinePerformIO (peekElemOff crc32_lut (fromIntegral i))
+{-# INLINE lut #-}
