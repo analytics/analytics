c84f60dd510098c740a07e407c7ac26e98e49154
Edward Kmett <ekmett@gmail.com>
2013-03-12 05:10:00 -0400

    Added a 'number of leading zeroes' calculation to 'Ranked'.

--- src/Data/Analytics/Bits.hs
+++ src/Data/Analytics/Bits.hs
@@ -11,6 +11,8 @@
 module Data.Analytics.Bits
   ( Ranked(..)
   , log2
+  , w8
+  , w16
   , w32
   , w64
   ) where
@@ -47,16 +49,30 @@ class (Num t, Bits t) => Ranked t where
   rank n = lsb n + 1
   {-# INLINE rank #-}
 
+  -- | Calculate the number of leading zeros.
+  nlz :: t -> Int
+
 instance Ranked Word64 where
   lsb n = fromIntegral $ go (shiftR ((n .&. (-n)) * 0x07EDD5E59A4E28C2) 58) where
     go :: Word64 -> Word8
     go i = inlinePerformIO $ peekElemOff debruijn_lsb64 (fromIntegral i)
   {-# INLINE lsb #-}
 
+  nlz x0 = popCount (complement x6) where
+     x1 = x0 .|. shiftR x0 1
+     x2 = x1 .|. shiftR x1 2
+     x3 = x2 .|. shiftR x2 4
+     x4 = x3 .|. shiftR x3 8
+     x5 = x4 .|. shiftR x4 16
+     x6 = x5 .|. shiftR x5 32
+  {-# INLINE nlz #-}
+
 instance Ranked Word32 where
   lsb n = fromIntegral $ go (shiftR ((n .&. (-n)) * 0x077CB531) 27) where
     go :: Word32 -> Word8
     go i = inlinePerformIO $ peekElemOff debruijn_lsb32 (fromIntegral i)
+  {-# INLINE lsb #-}
+
 {-
   rank n = fromIntegral $ go (shiftR ((n .&. (-n)) * 0x4279976B) 26) where
     go :: Word32 -> Word8
@@ -64,45 +80,93 @@ instance Ranked Word32 where
   {-# INLINE rank #-}
 -}
 
+  nlz x0 = popCount (complement x5) where
+     x1 = x0 .|. shiftR x0 1
+     x2 = x1 .|. shiftR x1 2
+     x3 = x2 .|. shiftR x2 4
+     x4 = x3 .|. shiftR x3 8
+     x5 = x4 .|. shiftR x4 16
+  {-# INLINE nlz #-}
+
+
 instance Ranked Word16 where
   lsb = lsb . w32
   {-# INLINE lsb #-}
+
   rank = rank . w32
   {-# INLINE rank #-}
+
+  nlz x0 = popCount (complement x4) where
+     x1 = x0 .|. shiftR x0 1
+     x2 = x1 .|. shiftR x1 2
+     x3 = x2 .|. shiftR x2 4
+     x4 = x3 .|. shiftR x3 8
+  {-# INLINE nlz #-}
 
 instance Ranked Word8 where
   lsb = lsb . w32
   {-# INLINE lsb #-}
+
   rank = rank . w32
   {-# INLINE rank #-}
+
+  nlz x0 = popCount (complement x3) where
+     x1 = x0 .|. shiftR x0 1
+     x2 = x1 .|. shiftR x1 2
+     x3 = x2 .|. shiftR x2 4
+  {-# INLINE nlz #-}
 
 instance Ranked Int64 where
   lsb = lsb . w64
   {-# INLINE lsb #-}
+
   rank = rank . w64
   {-# INLINE rank #-}
+
+  nlz = nlz . w64
+  {-# INLINE nlz #-}
 
 instance Ranked Int32 where
   lsb = lsb . w32
   {-# INLINE lsb #-}
+
   rank = rank . w32
   {-# INLINE rank #-}
+
+  nlz = nlz . w32
+  {-# INLINE nlz #-}
 
 instance Ranked Int16 where
   lsb = lsb . w32
   {-# INLINE lsb #-}
+
   rank = rank . w32
   {-# INLINE rank #-}
+
+  nlz = nlz . w16
+  {-# INLINE nlz #-}
 
 instance Ranked Int8 where
   lsb = lsb . w32
   {-# INLINE lsb #-}
+
   rank = rank . w32
   {-# INLINE rank #-}
+
+  nlz = nlz . w8
+  {-# INLINE nlz #-}
 
 ------------------------------------------------------------------------------
 -- Util
 ------------------------------------------------------------------------------
+
+w8 :: Integral a => a -> Word8
+w8 = fromIntegral
+{-# INLINE w8 #-}
+
+w16 :: Integral a => a -> Word16
+w16 = fromIntegral
+{-# INLINE w16 #-}
 
 w32 :: Integral a => a -> Word32
 w32 = fromIntegral
