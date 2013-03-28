15c82b7c0445300b946b60b3045f5f0cb094d2cf
Edward Kmett <ekmett@gmail.com>
2013-03-28 02:52:35 -0400

    Converted the arithmetic compressor to use the external bits project

--- src/Data/Analytics/Compression/Arithmetic.hs
+++ src/Data/Analytics/Compression/Arithmetic.hs
@@ -23,11 +23,13 @@ module Data.Analytics.Compression.Arithmetic
 import Control.Applicative
 import Control.Lens
 import Control.Monad
-import Data.Analytics.Compression.Bits
+import Control.Monad.Trans
 import Data.Bits
+import Data.Bits.Coding
 import Data.Bits.Lens
+import Data.Bytes.Get
+import Data.Bytes.Put
 import Data.Int
-import Data.Monoid
 import Data.Typeable
 
 ------------------------------------------------------------------------------
@@ -38,56 +40,68 @@ data Encoding = Encoding
   { _encodingLo      :: {-# UNPACK #-} !Int64
   , _encodingHi      :: {-# UNPACK #-} !Int64
   , _encodingScale   :: {-# UNPACK #-} !Int64
-  , _encodingBuilder :: !BitBuilder
   } deriving Typeable
 
 -- | @'encode' lo hi total@ will encode a symbol in our arithmetic encoding with weight @(hi - lo) >= 1@
 -- out of @total@ probability.
-encode :: Int64 -> Int64 -> Int64 -> Encoding -> Encoding
-encode !lc !hc !n (Encoding l h s o) = go (l + step * lc) (l + step * hc - 1) o
+encode :: MonadPut m => Int64 -> Int64 -> Int64 -> Encoding -> Coding m Encoding
+encode !lc !hc !n (Encoding l h s) = go (l + step * lc) (l + step * hc - 1)
   where
   !step = round (fromIntegral (h-l+1) / fromIntegral n :: Double)
-  go !ml !mh !mo
-    | mh < q2   = go (shiftL ml 1)      (shiftL mh 1 + 1)      (mo <> putBit False <> replicateBit s True)
-    | ml >= q2  = go (shiftL (ml-q2) 1) (shiftL (mh-q2) 1 + 1) (mo <> putBit True <> replicateBit s False)
-    | otherwise = e3 ml mh s mo
-  e3 !ml !mh !ms !mo
-    | q1 <= ml && mh < q3 = e3 (shiftL (ml-q1) 1) (shiftL (mh-q1) 1 + 1) (s + 1) mo
-    | otherwise           = Encoding ml mh ms mo
+  go !ml !mh
+    | mh < q2   = do
+      putBit False
+      replicateBit s True
+      go (shiftL ml 1) (shiftL mh 1 + 1)
+    | ml >= q2  = do
+      putBit True
+      replicateBit s False
+      go (shiftL (ml-q2) 1) (shiftL (mh-q2) 1 + 1)
+    | otherwise = e3 ml mh s
+  e3 !ml !mh !ms
+    | q1 <= ml && mh < q3 = e3 (shiftL (ml-q1) 1) (shiftL (mh-q1) 1 + 1) (s + 1)
+    | otherwise           = return $ Encoding ml mh ms
 
 -- | Determine any final trailing e3 bits.
-encoding :: Encoding -> BitBuilder
-encoding (Encoding l _ s o)
-  | l < q1    = o <> putBit False <> replicateBit (s + 1) True
-  | otherwise = o <> putBit True <> replicateBit (s + 1) False
+encoding :: MonadPut m => Encoding -> Coding m ()
+encoding (Encoding l _ s)
+  | l < q1    = putBit False >> replicateBit (s + 1) True
+  | otherwise = putBit True >> replicateBit (s + 1) False
 {-# INLINE encoding #-}
+
 
 ------------------------------------------------------------------------------
 -- Decoding
 ------------------------------------------------------------------------------
 
-data Decoding a = Decoding { runDecoding :: forall r. (a -> Int -> Int64 -> GetBits r) -> Int -> Int64 -> GetBits r }
+newtype Decoding m a = Decoding { runDecoding :: forall r. (a -> Int -> Int64 -> Coding m r) -> Int -> Int64 -> Coding m r }
 
-instance Functor Decoding where
+instance Functor (Decoding m) where
   fmap f (Decoding m) = Decoding $ \ k -> m (k . f)
   {-# INLINE fmap #-}
 
-instance Applicative Decoding where
+instance Monad m => Applicative (Decoding m) where
   pure a = Decoding $ \k -> k a
   {-# INLINE pure #-}
   (<*>) = ap
   {-# INLINE (<*>) #-}
 
-instance Monad Decoding where
+instance Monad m => Monad (Decoding m) where
   return a = Decoding $ \ k -> k a
   {-# INLINE return #-}
   Decoding m >>= f = Decoding $ \ k -> m $ \a -> runDecoding (f a) k
   {-# INLINE (>>=) #-}
 
+instance MonadTrans Decoding where
+  lift m = Decoding $ \k i b -> do
+    a <- lift m
+    k a i b
+  {-# INLINE lift #-}
+
 -- | For now fill all but the most significant bit directly.
 --
 -- This means we cannot arithmetic encode a sequence of bits shorter than 63 bits.
-decoding :: Decoding a -> GetBits a
+decoding :: MonadGet m => Decoding m a -> Coding m a
 decoding m = do
   startBits <- replicateM 63 getBit
   runDecoding m (\a _ _ -> return a) 63 $ ifoldrOf (backwards traversed) (set . bitAt) 0 startBits
@@ -101,8 +115,8 @@ q1 = 0x2000000000000000
 q2 = 0x6000000000000000
 q3 = 0x4000000000000000
 
-replicateBit :: Int64 -> Bool -> BitBuilder
-replicateBit 0 _ = mempty
-replicateBit n b = putBit b <> replicateBit (n - 1) b
+replicateBit :: MonadPut m => Int64 -> Bool -> Coding m ()
+replicateBit 0 _ = return ()
+replicateBit n b = putBit b >> replicateBit (n - 1) b
 {-# INLINE replicateBit #-}
 
