98b004ba7deadc50f283ede20dbf5254733b2433
Edward Kmett <ekmett@gmail.com>
2013-05-16 22:28:03 -0400

    Added msb and lsb calculations for Word64 values using bsf and bsr internally

--- src/Data/Analytics/Bits.hs
+++ src/Data/Analytics/Bits.hs
@@ -3,11 +3,15 @@ module Data.Analytics.Bits
   , reverseBytes
   , byteSum
   , byteCounts
+  , lsb, msb
   ) where
 
+import Control.Exception
 import Data.Bits
 import Data.Word
+import Foreign.C.Types
 
+-- | reverse the bits in a word
 reverseBits :: Word64 -> Word64
 reverseBits a = reverseBytes d where
   b = (shiftR a 1 .&. m1) .|. shiftR (a .&. m1) 1
@@ -18,6 +22,8 @@ reverseBits a = reverseBytes d where
   m3 = 0x0F0F0F0F0F0F0F0F
 
 foreign import ccall reverseBytes :: Word64 -> Word64
+foreign import ccall bsf64 :: Word64 -> CULong
+foreign import ccall bsr64 :: Word64 -> CULong
 
 -- | Calculate the total of the unsigned bytes of a 64-bit word
 byteSum :: Word64 -> Int
@@ -33,3 +39,11 @@ byteCounts a = d where
   d = (c + shiftR c 4) .&. (0x0f * bytes)
   nybbles = 0x1111111111111111
   bytes = 0x0101010101010101
+
+-- | return the position of the least significant set bit in a word, assumes that there is a set bit
+lsb :: Word64 -> Int
+lsb a = assert (a /= 0) $ fromIntegral $ bsf64 a
+
+-- | return the position of the most significant set bit in a word, assumes that there is a set bit
+msb :: Word64 -> Int
+msb a = assert (a /= 0) $ fromIntegral $ bsr64 a
