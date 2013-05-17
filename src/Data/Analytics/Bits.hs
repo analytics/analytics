5ed7687570bf9d4b5ef43f6882c69264b6ce154a
Edward Kmett <ekmett@gmail.com>
2013-05-16 23:19:12 -0400

    shift in the correct direction

--- src/Data/Analytics/Bits.hs
+++ src/Data/Analytics/Bits.hs
@@ -14,12 +14,12 @@ import Foreign.C.Types
 -- | reverse the bits in a word
 reverseBits :: Word64 -> Word64
 reverseBits a = reverseBytes d where
-  b = (shiftR a 1 .&. m1) .|. shiftR (a .&. m1) 1
-  c = (shiftR b 2 .&. m2) .|. shiftR (b .&. m2) 2
-  d = (shiftR c 4 .&. m3) .|. shiftR (c .&. m3) 4
   m1 = 0x5555555555555555
   m2 = 0x3333333333333333
   m3 = 0x0F0F0F0F0F0F0F0F
+  b = (shiftR a 1 .&. m1) .|. shiftL (a .&. m1) 1
+  c = (shiftR b 2 .&. m2) .|. shiftL (b .&. m2) 2
+  d = (shiftR c 4 .&. m3) .|. shiftL (c .&. m3) 4
 
 foreign import ccall reverseBytes :: Word64 -> Word64
 foreign import ccall bsf64 :: Word64 -> CULong
