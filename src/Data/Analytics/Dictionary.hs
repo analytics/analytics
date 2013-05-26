5cb4ea8748f220ac2288ff7037232b1262d472d0
Edward Kmett <ekmett@gmail.com>
2013-05-25 22:12:13 -0400

    Updated the specification for rank and select to make the retract situation hold for all `Int` args.

--- src/Data/Analytics/Dictionary.hs
+++ src/Data/Analytics/Dictionary.hs
@@ -19,39 +19,41 @@ module Data.Analytics.Dictionary
   , selectFoldable
   ) where
 
-
 import Data.Bits
 import Data.Analytics.Broadword
 import Data.Foldable as Foldable
-import Data.Vector as Vector
-import Data.Vector.Primitive as Primitive
-import Data.Vector.Unboxed as Unboxed
-import Data.Vector.Storable as Storable
+-- import Data.Vector as Vector
+-- import Data.Vector.Primitive as Primitive
+-- import Data.Vector.Unboxed as Unboxed
+-- import Data.Vector.Storable as Storable
 import Data.Sequence as Seq
 import Data.Word
 
 -- | This class is used to model succinct indexable dictionaries that support 'rank' and 'select' operations.
+--
 -- Since we can always implement one in terms of the other using galloping search if we know the 'size' of the structure,
 -- putting them together in one class seems appropriate.
 --
--- The meaning of operations when values are supplied out of range is 'undefined'. The tools here are enough to check
--- for valid ranges.
+-- We use an extended variant of these combinators that permits the following simple specification:
+--
+-- For @i@ >= @1@,
 --
 -- @
 -- 'rank' x xs ('select' x xs i) ≡ i
+-- 'select' x xs ('rank' x xs i) <= i
 -- @
 --
--- There is a Galois connection between 'rank' and 'select'.
+-- There is a Galois connection between 'rank' and 'select'. In fact, due to the equality, they form a \"retract situation\".
 --
--- For @i@ and @j@ in @[0..'size' xs)@,
+-- <http://ncatlab.org/nlab/show/retraction>
 --
--- @'rank' x xs i <= j@ iff @i <= 'select' x xs j@
+-- Given this and the well-foundedness of the total order on the natural numbers, 'select' can be defined in terms of 'rank' by
+-- binary search.
 --
--- Minimum complete definition: One of
+-- To simplify the upper bound on the above We also extend the definition of 'rank' and 'select' to consider every position
+-- after the end of the dictionary to match all symbols. This means we can implement 'rank' by binary search on 'select' as well.
 --
--- 1) 'size' and 'rank'
---
--- 2) 'size' and 'select'
+-- A minimum complete definition for this class requires 'size' and either 'rank' or 'select'.
 class Dictionary a t | t -> a where
   -- | An /O(n)/ default definition is supplied in terms of 'Foldable'.
   size :: t -> Int
@@ -61,79 +63,122 @@ class Dictionary a t | t -> a where
 
   -- | @'rank' x i xs@ computes the number of occurrences of @x@ in @xs@ in positions @[0..i)@
   --
-  -- For @i@ in @[0..'size' xs)@, the answer should lie in the range @[0..i)@.
+  -- If @i@ is larger than the length of the container, then every position past the end of the
+  -- data set is considered to match. This simplifies the statement of the 'select' and 'rank' laws.
   --
-  -- Answers for inputs outside of this range are 'undefined'.
+  -- Additionally, we choose the conservative extension:
+  --
+  -- @rank x xs i | i <= 0 = i@
+  --
+  -- Between these two extensions,
+  --
+  -- For @i@ >= @1@ the answer should lie in the range @[0..i]@, when @i@ <= @0@ then the answer will be @i@.
   --
   -- An /O('select' cost * log n)/ default definition is supplied in terms of 'select'.
+  --
+  -- >>> rank 1 [1,0,1] 2
+  -- 1
+  --
+  -- >>> rank 1 [1,0,1] 3
+  -- 2
   rank :: a -> t -> Int -> Int
-  rank a t i = search (\j -> select a t j >= i) 0 i
+  rank a t i
+    | i <= 0    = i
+    | otherwise = search (\j -> select a t j >= i) 0 i
   {-# INLINE rank #-}
 
-  -- | @deltaRank x i j xs@ computes the number of occurences of @x@ in @xs@ in the interval @[i..j)@.
+  -- | @deltaRank x xs i j@ computes the number of occurences of @x@ in @xs@ in the interval @[i..j)@.
   --
   -- The identity
   --
-  -- @'deltaRank' a i j xs = 'rank' a j xs - 'rank' a i xs@
+  -- @
+  -- 'deltaRank' a xs i j = 'rank' a xs j - 'rank' a xs i
+  -- @
   --
   -- is expected to hold, but it can often be implemented more efficiently.
   --
   -- The obvious default definition is supplied in terms of this identity.
+  --
+  -- A corrolary of this identity is that:
+  --
+  -- @
+  -- 'deltaRank' a xs 0 i = 'rank' a xs i
+  -- @
   deltaRank :: a -> t -> Int -> Int -> Int
   deltaRank a t i j = rank a t j - rank a t i
   {-# INLINE deltaRank #-}
 
-  -- @'select x xs i'@ returns the position of the @i@th occurence of @x@ in @xs@.
+  -- @'select x xs i'@ returns the position /after/ the @i@th occurence of @x@ in @xs@. You can also
+  -- view it as the @1@-based position of the @i@th occurence.
   --
-  -- This provides legal answers for @i@ in @[0..'rank' x xs ('size' xs))@ in the range
-  -- @[i..size xs)@. For @i >= 'rank' xs ('size' xs)@, this will return @'size' xs@ instead.
+  -- This provides legal answers for @i@ in @[1..'rank' x xs ('size' xs))@ in the range
+  -- @[i..size xs)@. For @i >= 'rank' xs ('size' xs)@, you'll get positions larger than @('size' xs)@
   --
-  -- @select x xs i | i >= 'rank' x xs ('size' xs) = 'size' xs@
+  -- @
+  -- select x xs i | i >= r = n + i - r where n = 'size' xs; r = 'rank' x xs n
+  -- @
+  --
+  -- Moreover,
+  --
+  -- @
+  -- select x xs i | i <= 0 = i
+  -- @
   --
   -- This permits 'rank' to be easily implemented by binary search using 'select'.
   --
   -- An /O('rank' cost * log n)/ default definition is supplied in terms of 'rank'.
+  --
+  -- >>> select 1 [1,0,1] 1
+  -- 1
+  --
+  -- >>> select 1 [1,0,1] 2
+  -- 3
+
+  -- >>> select 1 [1,0,1] 3
+  -- 4
   select :: a -> t -> Int -> Int
-  select a t i = search (\j -> rank a t j >= i) i (size t)
+  select a t i
+    | i <= 0 = i
+    | otherwise = search (\j -> rank a t j >= i) i (size t + i)
   {-# INLINE select #-}
 
+-- | This provides a valid default definition for 'select' for a 'Foldable' container. This is useful for testing,
+-- but not so practical for real world use, as it requires /O(n)/ time to execute a query.
 selectFoldable :: (Foldable f, Eq a) => a -> f a -> Int -> Int
-selectFoldable a xs k = go k 0 $ Foldable.toList xs where
+selectFoldable a xs k
+  | k <= 0 = k
+  | otherwise = go k 1 $ Foldable.toList xs
+  where
     go !i !j (b:bs)
       | a /= b = go i (j + 1) bs
-      | i <= 0 = j
+      | i <= 1 = j
       | otherwise = go (i - 1) (j + 1) bs
-    go _ j _ = j
+    go i j [] = i + j - 1
 {-# INLINE selectFoldable #-}
 
+-- | This provides a valid default definition for 'rank' for a 'Foldable' container. This is useful for testing,
+-- but not so practical for real world use, as it requires /O(n)/ time to execute a query.
 rankFoldable :: (Foldable f, Eq a) => a -> f a -> Int -> Int
-rankFoldable x xs k = fst $ Foldable.foldl' step (0,0) xs where
-  step ij@(i,j) y
-    | j >= k = ij
-    | !j' <- j + 1, !i' <- (if x == y then i + 1 else i) = (i',j')
+rankFoldable x xs k
+  | k <= 0    = k
+  | otherwise = case Foldable.foldl' step (0,0) xs of (i,j) -> i + k - min j k
+  where
+    step ij@(i,j) y
+      | j >= k = ij
+      | !j' <- j + 1, !i' <- (if x == y then i + 1 else i) = (i',j')
 {-# INLINE rankFoldable #-}
 
 -- | /O(n)/ 'rank' and 'select'
 instance Eq a => Dictionary a [a] where
   size = Prelude.length
-  {-# INLINE size #-}
-  rank x xs k0 = go 0 0 xs where
-    go !acc !p (y:ys)
-      | p >= k0   = acc
-      | x == y    = go (acc + 1) (p + 1) ys
-      | otherwise = go acc (p + 1) ys
-    go !acc _ [] = acc
-  {-# INLINE rank #-}
-  select a xs k = go k 0 xs where
-    go !i !j (b:bs)
-      | a /= b = go i (j + 1) bs
-      | i <= 0 = j
-      | otherwise = go (i - 1) (j + 1) bs
-    go _ j _  = j
-  {-# INLINE select #-}
+  rank = rankFoldable
+  select = selectFoldable
 
 -- | /O(n)/ 'rank' and 'select'
-instance Eq a => Dictionary a (Seq a)
+instance Eq a => Dictionary a (Seq a) where
+  size = Seq.length
+  rank = rankFoldable
+  select = selectFoldable
 
 search :: (Int -> Bool) -> Int -> Int -> Int
 search p l0 h0 = go l0 h0 where
@@ -158,54 +203,3 @@ instance Dictionary Bool Word64 where
   select True  xs i = selectWord64 xs i
   select False xs i = selectWord64 (complement xs) i
   {-# INLINE select #-}
-
--- | /O(n)/ 'rank' and 'select'
-instance Eq a => Dictionary a (Vector.Vector a)
-
--- | /O(n)/ 'rank' and 'select'
-instance (Eq a, Prim a) => Dictionary a (Primitive.Vector a) where
-  size = Primitive.length
-  {-# INLINE size #-}
-  rank x xs k = fst $ Primitive.foldl' step (0,0) xs where
-    step ij@(i,j) y
-      | j >= k    = ij
-      | !j' <- j + 1, !i' <- (if x == y then i + 1 else i) = (i',j')
-  {-# INLINE rank #-}
-  select a xs k = go k 0 $ Primitive.toList xs where
-    go !i !j (b:bs)
-      | a /= b = go i (j + 1) bs
-      | i <= 0 = j
-      | otherwise = go (i - 1) (j + 1) bs
-    go _ j _ = j
-
--- | /O(n)/ 'rank' and 'select'
-instance (Eq a, Unbox a) => Dictionary a (Unboxed.Vector a) where
-  size = Unboxed.length
-  {-# INLINE size #-}
-  rank x xs k = fst $ Unboxed.foldl' step (0,0) xs where
-    step ij@(i,j) y
-      | j >= k    = ij
-      | !j' <- j + 1, !i' <- (if x == y then i + 1 else i) = (i',j')
-  {-# INLINE rank #-}
-  select a xs k = go k 0 $ Unboxed.toList xs where
-    go !i !j (b:bs)
-      | a /= b = go i (j + 1) bs
-      | i <= 0 = j
-      | otherwise = go (i - 1) (j + 1) bs
-    go _ j _ = j
-
--- | /O(n)/ 'rank' and 'select'
-instance (Eq a, Storable a) => Dictionary a (Storable.Vector a) where
-  size = Storable.length
-  {-# INLINE size #-}
-  rank x xs k = fst $ Storable.foldl' step (0,0) xs where
-    step ij@(i,j) y
-      | j >= k    = ij
-      | !j' <- j + 1, !i' <- (if x == y then i + 1 else i) = (i',j')
-  {-# INLINE rank #-}
-  select a xs k = go k 0 $ Storable.toList xs where
-    go !i !j (b:bs)
-      | a /= b = go i (j + 1) bs
-      | i <= 0 = j
-      | otherwise = go (i - 1) (j + 1) bs
-    go _ j _ = j
